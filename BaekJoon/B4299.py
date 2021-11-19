9b7bf592f025dcc73c262ce92b133bec2e68c742
greatjade24 <greatjade24@naver.com>
2021-11-19 10:07:26 +0900

    BaekJoon code

--- BaekJoon/B4299.py
+++ BaekJoon/B4299.py
@@ -6,7 +6,7 @@ n, m = map(int, input().split())
 a = (n + m) // 2
 b = (n - m) // 2
 
-if a + b != n or a - b != m or n < m:
+if a + b != n or a - b != m or n < m:   # n < m 조건을 추가하지 않으면 두 경기의 합이 차보다 작을 경우 -1을 출력하지 않는 반례가 생성됨
     print(-1)
 else:
     print(a, b)
